eaed95d82bfebd83077eefa8d0a289d4e176cf82
rohinvaidya <48517346+rohinvaidya@users.noreply.github.com>
2026-03-14 16:36:29 -0400

    CORS Middleware

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from app.routes.issues import router as issues_router
 from app.middleware.timer import timing_middleware
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.middleware("http")(timing_middleware)
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"],
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"])
 
 app.include_router(issues_router)
 
